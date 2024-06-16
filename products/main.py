43e581c76e2efeff95834fd38ad1617a6d2896dd
scottlusk <scottlusk.cis@outlook.com>
2024-06-16 17:04:26 -0400

    remove info endpoint

--- products/main.py
+++ products/main.py
@@ -5,17 +5,13 @@ from . import config
 
 app = FastAPI()
 
+
 @lru_cache
 def get_settings() -> config.Settings:
     return config.Settings()
 
+
 @app.get("/")
 async def root() -> dict[str, str]:
     return {"message": "Hello World"}
-  
-@app.get("/info")
-async def info(settings: Annotated[config.Settings, Depends(get_settings)]):
-    return {
-      "account_uri": settings.account_uri,
-      "account_key": settings.account_key,
-    }
+
